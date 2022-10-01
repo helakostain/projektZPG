bc73fd1f64b989685adb3e49939d701643f829f8
Adam Havranek <adamhavra@seznam.cz>
2022-10-01 18:42:52 +0200

    Transfered some stuff from main to Application

--- projektZPG/Application.cpp
+++ projektZPG/Application.cpp
@@ -1,9 +1,44 @@
 #include "Application.h"
 
+float Application::points[] = {
+	 0.0f, 0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	-0.5f, -0.5f, 0.0f
+};
+
 Application::Application()
 {
+	Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.01f, 100.0f);
+	View = glm::lookAt(
+		glm::vec3(10, 10, 10), // Camera is at (4,3,-3), in World Space
+		glm::vec3(0, 0, 0), // and looks at the origin
+		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+	);
+	Model = glm::mat4(1.0f);
+	vertex_shader =
+		"#version 330\n"
+		"layout(location=0) in vec3 vp;"
+		"void main () {"
+		"     gl_Position = vec4 (vp, 1.0);"
+		"}";
+	fragment_shader =
+		"#version 330\n"
+		"out vec4 frag_colour;"
+		"void main () {"
+		"     frag_colour = vec4 (0.0, 0.5, 1.0, 1.0);"
+		"}";
 }
 
 Application::~Application()
 {
+}
+
+void Application::error_callback(int error, const char* description) 
+{
+	fputs(description, stderr);
+}
+
+void Application::Run()
+{
+
 }
--- projektZPG/Application.h
+++ projektZPG/Application.h
@@ -1,11 +1,40 @@
 #pragma once
+//Include GLEW
+#include <GL/glew.h>
+//Include GLFW
+#include <GLFW/glfw3.h>
+
+//Include GLM  
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include <glm/gtc/type_ptr.hpp> // glm::value_ptr
+
+//Include the standard C++ headers  
+#include <stdlib.h>
+#include <stdio.h>
 
 class Application
 {
 private:
+	// Projection matrix : 45 Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection;
+	// Camera matrix
+	glm::mat4 View;
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model;
 
+	static float points[];
 
+	const char* vertex_shader;
+
+	const char* fragment_shader;
 public:
 	Application();
 	~Application();
+
+	static void error_callback(int error, const char* description) { fputs(description, stderr); }
+
+	void Run();
 };
