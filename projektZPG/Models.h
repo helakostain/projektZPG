8a891d4ba012003409e39bd3c4cdd2b400c036d4
helak <adamhavra@seznam.cz>
2022-10-08 18:32:51 +0200

    new triangle

--- projektZPG/Models.h
+++ projektZPG/Models.h
@@ -24,6 +24,7 @@ private:
 	static GLuint VAO; //Vertex Array Object (VAO)
 
 	static float points[]; //quad for display with colours
+	static float point2[];
 
 	static void GenerateVBO();
 	static void GenerateVAO();
